590fb25171d9fa7e0a3ef5fbc539289694924b18
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-08 21:21:09 +0600

    [refactor]: change server name to `kafka` for listener;

--- KafkaListener.java
+++ KafkaListener.java
@@ -6,5 +6,5 @@ public @interface KafkaListener {
     String topic();
     String groupId();
     // String bootstrapServers() default "localhost:9092";
-    String bootstrapServers() default "kafka-cluster:9092";
+    String bootstrapServers() default "kafka:9092";
 }
